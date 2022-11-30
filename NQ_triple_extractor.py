25ccf7b38f5611a3a8989fb4ffcad0ace8a01ba0
HongGiwon <gch02518@kaist.ac.kr>
2022-11-30 09:05:40 +0000

    error handling due to connection problems

--- NQ_triple_extractor.py
+++ NQ_triple_extractor.py
@@ -171,3 +171,21 @@ for NQ_idx, NQ_ins in enumerate(tqdm(NQ_data)):
         error_list.append(NQ_idx)
         NQ_c2c_triples.append([])
         NQ_q2c_triples.append([])
+
+# error handling
+batch_size = 20
+for NQ_idx in tqdm(error_list):
+    print(NQ_idx)
+    Q_entity = dict_set_query_test_ent[NQ_idx]
+    C_entity_batch = set()
+    for NQ_ins_ctx_idx, NQ_ins_ctx in enumerate(NQ_data[NQ_idx]['ctxs'][:10]):
+        C_entity_batch.update(dict_set_ctx_ent_tdidf[NQ_ins_ctx['id']])
+    result_Q = wikidata_triple_retrieve(Q_entity, C_entity_batch)
+    result_Q += wikidata_triple_retrieve(C_entity_batch, Q_entity)
+    
+    result_C = []
+    C_entity_batch_list = list(C_entity_batch)
+    for i in range(int(len(C_entity_batch) / batch_size) + bool(len(C_entity_batch) % batch_size)):
+        result_C += wikidata_triple_retrieve(C_entity_batch, C_entity_batch_list[i*batch_size:(i+1)*batch_size])
+    NQ_c2c_triples[NQ_idx] += result_C
+    NQ_q2c_triples[NQ_idx] += result_Q
