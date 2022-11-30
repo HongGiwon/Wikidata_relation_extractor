d8bdff2ae0e9810f7be20a13bd3615925cff9067
HongGiwon <gch02518@kaist.ac.kr>
2022-11-30 08:37:14 +0000

    entity_linking data split

--- entity_linking.py
+++ entity_linking.py
@@ -10,33 +10,37 @@ from json import JSONDecodeError
 nlp = spacy.load("en_core_web_md")
 nlp.add_pipe("entityLinker", last=True)
 
-dict_query_train_ent = {}
-dict_query_dev_ent = {}
-dict_query_test_ent = {}
+dict_query_ent = {}
 dict_ctx_ent = {}
 dict_title2id = defaultdict(list)
 
-with open('./NQ/test.json', 'r') as f:
-    NQ_data = json.load(f)
-    
-for NQ_idx, NQ_ins in enumerate(tqdm(NQ_data)):
-    
-    #question el
-    dict_query_train_ent[NQ_idx] = []
-    NQ_question = NQ_ins['question']
-    doc_el = nlp(NQ_question)
-    for ent in doc_el._.linkedEntities:
-        dict_query_train_ent[NQ_idx].append(ent.get_id())
-        
-    #ctx el
-    for NQ_ins_ctx in NQ_ins['ctxs']:
-        if NQ_ins_ctx['id'] in dict_ctx_ent:
-            continue
-        dict_title2id[NQ_ins_ctx['title']].append(NQ_ins_ctx['id'])
-        dict_ctx_ent[NQ_ins_ctx['id']] = []
-        doc_el = nlp(NQ_ins_ctx['text'])
-        for ent in doc_el._.linkedEntities:
-            dict_ctx_ent[NQ_ins_ctx['id']].append(ent.get_id())
+for filename in ["test", "dev", "train"][:1]:
 
-with open('./dict_query_train_ent.pkl', 'wb') as f:
-    pickle.dump(dict_query_train_ent, f)
+    with open('./NQ/' + filename + '.json', 'r') as f:
+        NQ_data = json.load(f)
+
+    for NQ_idx, NQ_ins in enumerate(tqdm(NQ_data)):
+
+        #question el
+        dict_query_ent[NQ_idx] = []
+        NQ_question = NQ_ins['question']
+        doc_el = nlp(NQ_question)
+        for ent in doc_el._.linkedEntities:
+            dict_query_ent[NQ_idx].append(ent.get_id())
+
+        #ctx el
+        for NQ_ins_ctx in NQ_ins['ctxs']:
+            if NQ_ins_ctx['id'] in dict_ctx_ent:
+                continue
+            dict_title2id[NQ_ins_ctx['title']].append(NQ_ins_ctx['id'])
+            dict_ctx_ent[NQ_ins_ctx['id']] = []
+            doc_el = nlp(NQ_ins_ctx['text'])
+            for ent in doc_el._.linkedEntities:
+                dict_ctx_ent[NQ_ins_ctx['id']].append(ent.get_id())
+
+    with open('./dict_query_' + filename + '_ent.pkl', 'wb') as f:
+        pickle.dump(dict_query_ent, f)
+with open('./dict_ctx_ent.pkl', 'wb') as f:
+    pickle.dump(dict_ctx_ent, f)
+with open('./dict_title2id.pkl', 'wb') as f:
+    pickle.dump(dict_title2id, f)
